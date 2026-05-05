32bf0f773d58dc939be62f89eac36846f2241e9f
FBEZ <francesco.bez@espressif.com>
2026-05-05 08:26:33 +0200

    added

--- content/blog/2026/05/simple_provisioning/main/main.c
+++ content/blog/2026/05/simple_provisioning/main/main.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 #include <string.h>
-
 #include <freertos/FreeRTOS.h>
 #include <freertos/task.h>
 #include <freertos/event_groups.h>
-
 #include <esp_log.h>
 #include <esp_wifi.h>
 #include <esp_event.h>
